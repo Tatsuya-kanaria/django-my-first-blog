2f3411af799e75ba3c15cf3b3c027df7647dc102
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-12-12 14:32:20 +0900

    Added views to create/edit blog post inside the site.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+import re
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -13,3 +16,36 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_new(request):
+    title = "new"
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, "blog/post_edit.html", {"form": form, "title": title})
+
+
+def post_edit(request, pk):
+    title = "edit"
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "blog/post_edit.html", {"form": form, "title": title})
